49f626393b29011bdd0f393a8d907916170d6121
Gül Sena Altıntaş <30217912+gsaltintas@users.noreply.github.com>
2025-07-06 12:13:44 +0300

    Updated data download scripts to include our subsets

--- setup/download_prepare_hf_data.py
+++ setup/download_prepare_hf_data.py
@@ -1,9 +1,11 @@
 # Copyright (c) Meta Platforms, Inc. and affiliates.
 
 import argparse
+import math
 import os
-import time
 import subprocess
+import time
+
 import requests
 from huggingface_hub import snapshot_download
 
@@ -37,13 +39,42 @@ def download_dataset(repo_id, local_dir, allow_patterns):
     print(f"Dataset downloaded to {local_dir}")
 
 
-def parquet_to_jsonl(dataset, work_dir, src_dir, tgt_dir, ntasks=64):
+def parquet_to_jsonl(
+    dataset, work_dir, src_dir, tgt_dir, ntasks=64, preserve_subsets=False
+):
     from datatrove.executor import LocalPipelineExecutor
     from datatrove.pipeline.readers import ParquetReader
     from datatrove.pipeline.writers import JsonlWriter
 
-    pipeline_exec = LocalPipelineExecutor(
-        pipeline=[
+    if preserve_subsets:
+        subsets = os.listdir(f"{src_dir}/data")
+        pipe = []
+        for subset in subsets:
+            print(f"Processing subset: {subset}")
+            # pipe.extend(
+            pipe = [
+                ParquetReader(
+                    f"{src_dir}/data/{subset}/",
+                    file_progress=True,
+                    doc_progress=True,
+                    glob_pattern="**/*.parquet",
+                ),
+                JsonlWriter(
+                    tgt_dir,
+                    output_filename=f"{dataset}.{subset}" + ".chunk.${rank}.jsonl",
+                    compression=None,
+                ),
+            ]
+            pipeline_exec = LocalPipelineExecutor(
+                pipeline=pipe,
+                tasks=ntasks,
+                logging_dir=os.path.join(work_dir, "datatrove", subset),
+                # skip_completed=False,
+            )
+            pipeline_exec.run()
+        return
+    else:
+        pipe = [
             ParquetReader(
                 src_dir,
                 file_progress=True,
@@ -55,9 +86,10 @@ def parquet_to_jsonl(dataset, work_dir, src_dir, tgt_dir, ntasks=64):
                 output_filename=dataset + ".chunk.${rank}.jsonl",
                 compression=None,
             ),
-        ],
-        tasks=ntasks,
-        logging_dir=os.path.join(work_dir, "datatrove"),
+        ]
+
+    pipeline_exec = LocalPipelineExecutor(
+        pipeline=pipe, tasks=ntasks, logging_dir=os.path.join(work_dir, "datatrove")
     )
     pipeline_exec.run()
 
@@ -76,11 +108,14 @@ def setup_terashuf(work_dir):
     return terashuf_dir
 
 
-def main(dataset, memory, data_dir, seed=42, nchunks=32):
+def main(dataset, memory, data_dir, seed=42, nchunks=32, preserve_subsets=False):
     # Configuration
     repo_id = {
         "fineweb_edu": "HuggingFaceFW/fineweb-edu",
+        "fineweb_2": "HuggingFaceFW/fineweb-2",
+        "fineweb_2_hq": "epfml/FineWeb2-HQ",
         "fineweb_edu_10bt": "HuggingFaceFW/fineweb-edu",
+        "fineweb_edu_100bt": "HuggingFaceFW/fineweb-edu",
         "dclm_baseline_1.0": "mlfoundations/dclm-baseline-1.0",
         "dclm_baseline_1.0_10prct": "mlfoundations/dclm-baseline-1.0",
     }[dataset]
@@ -91,19 +126,51 @@ def main(dataset, memory, data_dir, seed=42, nchunks=32):
     prefix = f"{dataset}.chunk."
     orig_extension = {
         "fineweb_edu": ".jsonl",
+        "fineweb_2": ".jsonl",
+        "fineweb_2_hq": ".jsonl",
         "fineweb_edu_10bt": ".jsonl",
+        "fineweb_edu_100bt": ".jsonl",
         "dclm_baseline_1.0": ".jsonl.zst",
         "dclm_baseline_1.0_10prct": ".jsonl.zst",
     }[dataset]
     cat_command = {
         "fineweb_edu": "cat {}",
+        "fineweb_2": "cat {}",
+        "fineweb_2_hq": "cat {}",
         "fineweb_edu_10bt": "cat {}",
+        "fineweb_edu_100bt": "cat {}",
         "dclm_baseline_1.0": "zstdcat {} && echo",
         "dclm_baseline_1.0_10prct": "zstdcat {} && echo",
     }[dataset]
     allow_patterns = {
         "fineweb_edu": None,
         "fineweb_edu_10bt": "sample/10BT/*",
+        "fineweb_edu_100bt": "sample/100BT/*",
+        "fineweb_2": [
+            "data/arb_Arab/*/000_0000[01].parquet",
+            "data/ben_Beng/*/000_00000.parquet",
+            "data/eng_Latn/*/000_0000[01].parquet",
+            "data/deu_Latn/*/000_0000[01].parquet",
+            "data/fra_Latn/*/000_0000[01].parquet",
+            "data/ita_Latn/*/000_0000[01].parquet",
+            "data/hin_Deva/*/000_00000.parquet",
+            "data/jpn_Jpan/*/000_0000[01].parquet",
+            "data/kor_Hang/*/000_0000[01].parquet",
+            "data/rus_Cyrl/*/000_0000[012].parquet",
+            "data/spa_Latn/*/000_0000[01].parquet",
+            "data/swh_Latn/*/000_00000.parquet",
+            "data/tel_Telu/*/000_00000.parquet",
+            "data/tha_Thai/*/000_0000[01].parquet",
+            "data/tur_Latn/*/000_0000[01].parquet",
+            "data/cmn_Hani/*/000_0000[012].parquet",
+        ],
+        "fineweb_2_hq": [
+            "eng_Latn/*",  # /000_0000[01].parquet",
+            "ita_Latn/*",  # /000_0000[01].parquet",
+            "tur_Latn/*",  # /000_0000[01].parquet",
+            "fas_Arab/*",  # /000_0000[01].parquet",
+            "cmn_Hani/*",  # /000_0000[012].parquet",
+        ],
         "dclm_baseline_1.0": "*.jsonl.zst",
         "dclm_baseline_1.0_10prct": "global-shard_01_of_10/*.jsonl.zst",
     }[dataset]
@@ -117,7 +184,9 @@ def main(dataset, memory, data_dir, seed=42, nchunks=32):
     download_dataset(repo_id, src_dir, allow_patterns)
 
     if "fineweb" in dataset:
-        parquet_to_jsonl(dataset, work_dir, src_dir, src_dir)
+        parquet_to_jsonl(
+            dataset, work_dir, src_dir, src_dir, preserve_subsets=preserve_subsets
+        )
 
     # Set up environment variables
     os.environ["MEMORY"] = f"{memory}"
@@ -125,19 +194,40 @@ def main(dataset, memory, data_dir, seed=42, nchunks=32):
 
     # Run the original shuffling and splitting command
     terashuf_executable = os.path.join(terashuf_dir, "terashuf")
-    run_command(
-        f"ulimit -n 100000 && "
-        f"find {src_dir} -type f -name '*{orig_extension}' -print0 | xargs -0 -I {{}} sh -c '{cat_command}' | {terashuf_executable} | "
-        f"split -n r/{nchunks} -d --suffix-length 2 --additional-suffix {suffix} - {out_dir}/{prefix}"
-        "; trap 'echo \"Caught signal 13, exiting with code 1\"; exit 1' SIGPIPE;"
-    )
+    print(orig_extension, src_dir, cat_command, terashuf_executable)
+    if preserve_subsets:
+        for subset in os.listdir(f"{src_dir}/data"):
+            prefix = f"{dataset}.{subset}.chunk."
+            # Create validation set and remove lines from chunks
+            validation_file = f"{out_dir}/{subset}/{dataset}.{subset}.val{suffix}"
+            for i in range(nchunks):
+                chunk_file = f"{out_dir}/{subset}/{prefix}{i:02d}{suffix}"
+                run_command(f"head -n {k_validation} {chunk_file} >> {validation_file}")
+                run_command(f"sed -i '1,{k_validation}d' {chunk_file}")
+            continue
+            if not ("cmn" in subset):
+                continue
+            run_command(
+                f"mkdir -p {out_dir}/{subset} && "
+                f"ulimit -n 100000 && "
+                f"find {src_dir} -type f -name '*{subset}*{orig_extension}' -print0 | xargs -0 -I {{}} sh -c '{cat_command}' | {terashuf_executable} | "
+                f"split -n r/{nchunks} -d --suffix-length 2 --additional-suffix {suffix} - {out_dir}/{subset}/{prefix}"
+                # "; trap 'echo \"Caught signal 13, exiting with code 1\"; exit 1' SIGPIPE;"
+            )
+    else:
+        run_command(
+            f"ulimit -n 100000 && "
+            f"find {src_dir} -type f -name '*{orig_extension}' -print0 | xargs -0 -I {{}} sh -c '{cat_command}' | {terashuf_executable} | "
+            f"split -n r/{nchunks} -d --suffix-length 2 --additional-suffix {suffix} - {out_dir}/{prefix}"
+            "; trap 'echo \"Caught signal 13, exiting with code 1\"; exit 1' SIGPIPE;"
+        )
 
-    # Create validation set and remove lines from chunks
-    validation_file = f"{out_dir}/{dataset}.val{suffix}"
-    for i in range(nchunks):
-        chunk_file = f"{out_dir}/{prefix}{i:02d}{suffix}"
-        run_command(f"head -n {k_validation} {chunk_file} >> {validation_file}")
-        run_command(f"sed -i '1,{k_validation}d' {chunk_file}")
+        # Create validation set and remove lines from chunks
+        validation_file = f"{out_dir}/{dataset}.val{suffix}"
+        for i in range(nchunks):
+            chunk_file = f"{out_dir}/{prefix}{i:02d}{suffix}"
+            run_command(f"head -n {k_validation} {chunk_file} >> {validation_file}")
+            run_command(f"sed -i '1,{k_validation}d' {chunk_file}")
 
     print("All tasks completed successfully!")
 
@@ -149,7 +239,20 @@ if __name__ == "__main__":
     parser.add_argument("--data_dir", type=str, default="data")
     parser.add_argument("--seed", type=int, default=42)
     parser.add_argument("--nchunks", type=int, default=32)
+    parser.add_argument(
+        "--preserve_subsets",
+        action="store_true",
+        default=False,
+        help="If true, the subsets will be preserved in the output directory",
+    )
 
     args = parser.parse_args()
 
-    main(args.dataset, args.memory, args.data_dir, args.seed, args.nchunks)
+    main(
+        args.dataset,
+        args.memory,
+        args.data_dir,
+        args.seed,
+        args.nchunks,
+        args.preserve_subsets,
+    )
